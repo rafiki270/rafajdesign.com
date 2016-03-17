f8ac70b0e0735e580b497d00d130bef973df3f0c
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2016-03-17 23:44:22 +0000

    wip

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<meta name="robots" content="index, follow"/>
+	<title>Rafaj Design - Concrete furniture &amp; Garden design</title>
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Google Font Roboto -->
+	<link href="http://fonts.googleapis.com/css?family=Roboto:500,400italic,100,300,700,100italic,300italic,400" rel="stylesheet" type="text/css">
+	<!-- Bootstrap 3.3.5 -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<!-- Design Style MyWALL -->
+	<link rel="stylesheet" type="text/css" href="css/mywall.min.css" />
+	
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+	<!-- Modal Window Contact me -->
+	<div class="modal fade" id="callmeModal" tabindex="-1" aria-hidden="true" role="dialog" aria-labelledby="ModalCall" style="display: none;">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+				<h2 class="modal-title getmetelf" id="ModalCall">Get in touch</h2>
+				</div>
+				<div class="modal-body">
+					<!-- Your phone number -->
+					<div class="myphone">+44 797 257 4949</div>
+					<!-- Contact hours -->
+					<div class="contacthours">
+						<p class="hourcall">Contact hours</p>
+						<div class="rightcall">
+							<p class="dayscall">Mon - Fri</p>
+							<p class="horario">9:00 - 22:00</p>
+							<p class="dayscall">Sat - Sun</p>
+							<p class="horario">10:00 - 18:00</p>
+						</div>
+					</div>
+					<!-- // Contact hours -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // Modal Window Contact me -->
+
+	<!-- Header -->
+	<header>
+		<!-- Menu Navigation -->
+		<div id="top" class="container">
+			<!-- Menu left -->
+			<nav id="ha-header" class="left-nav ha-header">
+				<ul class="nav">
+			      <li class="dropdown">
+			      	<!-- Dropdown menu -->
+			        <a class="dropdown-toggle circle-main" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">        
+						<svg class="hello" height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg">
+						    <path d="M2 15.5v2h20v-2H2zm0-5v2h20v-2H2zm0-5v2h20v-2H2z"/>
+						    <path d="M0 0h24v24H0z" fill="none"/>
+						</svg>
+			        </a>
+			        <!-- Navigation Sections -->
+			        <ul class="dropdown-menu">
+						<li><a data-scroll data-options='{ "easing": "easeOutQuint" }' href="#top" class="menu-item wall">Hello</a></li>
+						<li><a data-scroll data-options='{ "easing": "easeOutQuint" }' href="#about" class="menu-item wall">About Us</a></li>
+						<li><a data-scroll data-options='{ "easing": "easeOutQuint" }' href="#portfolio" class="menu-item wall">Portfolio</a></li>
+						<li><a data-scroll data-options='{ "easing": "easeOutQuint" }' href="#download" class="menu-item wall">Social Networks</a></li>
+						<li><a data-scroll data-options='{ "easing": "easeOutQuint" }' href="#contact" class="menu-item wall">Contact Us</a></li>
+			        </ul>
+			      </li>
+			    </ul>
+			</nav>
+			<!-- // Menu left -->
+
+			<!-- Menu right - Hidden in small devices -->
+			<div class="nav-extra hidden-xs">
+				<!-- Button Contact Modal -->
+				<a class="callme" href="#" data-toggle="modal" data-target="#callmeModal">
+					<span class="glyphicon glyphicon glyphicon-earphone call" aria-hidden="true"></span>
+				</a>
+				<!-- Button 'Hire me' -->
+				<a class="hireme button-mywall" data-scroll data-options='{ "easing": "easeOutQuint" }' href="#contact">Hire us now</a>
+			</div>
+			<!-- // Menu right -->
+		</div>
+		<!-- // Menu Navigation -->
+
+		<!-- Personal Details -->
+		<div class="container">
+			<img class="img-circle imgsus" alt="Andrey Marin" src="img/profile.png"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<img class="img-circle imgsus" alt="Andrey Marin" src="img/profile.png"/>
+			<h1 class="name-boss">Rafaj Design</h1>
+			<h2 class="main-details">Gardens. Concrete. Design. Gizmos.</h2>
+			<p class="extend-details">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+		</div>
+		<!-- // Personal Details -->
+	</header>
+	<!-- // Header -->
+	
+	<!-- About Me Intro - Text Type - Hi. I'm ... -- >
+	<section id="about" class="cd-intro">
+		<h1 class="cd-headline letters type">
+			<span>Hi. </span>
+			<span class="cd-words-wrapper waiting">
+				<b class="is-visible">I am your garden</b>
+				<b>I want to look pretty</b>
+				<b>I could do with a makeover</b>
+			</span>
+		</h1>
+	</section>
+	<!-- // About Me Intro -->
+	
+	<p>&nbsp;</p>
+	
+	<!-- About Me Context -->
+	<section id="about" class="full-section my-about">
+		<!-- Top About me -->
+		<article class="container">
+			<div class="row">
+				<!-- Text about me -->
+				<div class="col-md-6">
+					<h2 class="title-main">About Us.</h2>
+					<p class="text-main">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+					<br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+				<!-- Image right about me -->
+				<div class="col-md-6">
+					<img class="img-responsive" src="img/pexels-work.jpg" alt="working">
+				</div>
+			</div>
+		</article>
+		<!-- // Top About me -->
+
+	</section>
+	<!-- // About Me Context -->
+
+
+
+	<!-- Portfolio -->
+	<section id="portfolio" class="full-section">
+		<!-- Gallery -->
+		<div class="container">
+			<ul class="row wall-portfolio">
+				<li class="col-md-4 doblock"><a href="img/item-1.png" data-imagelightbox="b"><img class="wall-gallery" src="img/item-1.png" alt="my item 1" /></a></li>
+				<li class="col-md-4 doblock"><a href="img/item-2.png" data-imagelightbox="b"><img class="wall-gallery" src="img/item-2.png" alt="my item 2" /></a></li>
+				<li class="col-md-4 doblock"><a href="img/item-3.png" data-imagelightbox="b"><img class="wall-gallery" src="img/item-3.png" alt="my item 3" /></a></li>
+				<li class="col-md-4 doblock"><a href="img/item-4.png" data-imagelightbox="b"><img class="wall-gallery" src="img/item-4.png" alt="my item 4" /></a></li>
+				<li class="col-md-4 doblock"><a href="img/item-5.png" data-imagelightbox="b"><img class="wall-gallery" src="img/item-5.png" alt="my item 5" /></a></li>
+				<li class="col-md-4 doblock"><a href="img/item-6.png" data-imagelightbox="b"><img class="wall-gallery" src="img/item-6.png" alt="my item 6" /></a></li>
+			</ul>
+		</div>
+		<!-- // Gallery -->
+	</section>
+	<!-- // Portfolio -->
+
+	<!-- Social and Download -->
+	<section id="download" class="full-download">
+		<div class="container">
+			<div class="row">
+				<!-- Social Networks -->
+				<div class="col-sm-12 btn-class col-md-7">
+					<a href="#" class="social-ink">
+						<img class="socl" src="img/twitter-icon.png" alt="twitter"/>
+					</a>
+					<a href="#" class="social-ink">
+						<img class="socl" src="img/facebook-icon.png" alt="facebook"/>
+					</a>
+					<a href="#" class="social-ink">
+						<img class="socl" src="img/google-icon.png" alt="google"/>
+					</a>
+					<a href="#" class="social-ink">
+						<img class="socl" src="img/linkedin-icon.png" alt="linkedin"/>
+					</a>
+				</div>
+				<!-- Download Resume - CV -->
+				<div class="col-sm-5 btn-class hidden-sm hidden-xs">
+					<a class="btn-dwnld" href="#">Download Portfolio</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- // Social and Download -->
+
+	<!-- Contact me -->
+	<section id="contact" class="full-section-three bg-contact">
+		<div class="container">
+			<!-- Title Contact me -->
+			<h1 class="title-ct">Contact us.</h1>
+			<p class="separator-ct"></p>
+			<!-- Localization -->
+			<div class="row">
+				<div class="col-md-12 col-xs-12 txt-center">
+					<p class="text-ct">info@rafajdesign.com</p>
+				</div>
+			</div>
+		</div>
+		<!-- Contact Form -- >
+		<article class="container cntme">
+			<form method="post" action="/envato/mywall/one/send-email.php" id="ContactForm" name="ContactForm" class="validate-form">
+				<div class="row">
+					<div class="form-group col-md-6 col-md-offset-3">
+					  <input type="text" class="form-control ct-name" name="name" id="name" placeholder="Your name" required>
+					</div>
+					<div class="form-group col-md-6 col-md-offset-3">
+					  <input type="email" class="form-control ct-mail" name="email" id="email" placeholder="Email" required>
+					</div>
+					<div class="form-group col-md-6 col-md-offset-3">
+					  <textarea class="form-control ct-message" rows="6" name="message" id="message" placeholder="Write anything here..." required></textarea>
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col-md-6 col-md-offset-3">
+						<div id="successmsg"></div>
+						<button type="submit" name="button" id="button" class="btn btnc2 with-br btn-ctc"><span>Send Now</span></button>
+					</div>
+				</div>
+			</form>
+		</article>
+		<!-- // Contact Form -->
+	</section>
+	<!-- // Contact me -->
+
+	<!-- Footer -->
+	<footer class="full-footer">
+		<div class="container padding-footer">
+			<!-- Text Left -->
+			<div class="row">
+				<div class="col-sm-8 foo-left">Let us help you be more happy. <br> Katerina &amp; Ondrej Rafaj</div>
+				<!-- Text Right -->
+				<div class="col-sm-4 foo-right">Made with <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> in London</div>
+			</div>
+		</div>
+	</footer>
+	<!-- // Footer -->
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Smooth-scroll -->
+	<script src="js/smooth-scroll.min.js"></script>
+	<!-- "Hi. I'am..." Text type - Resource jQuery -->
+	<script src="js/main.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Menu Scrolling -->
+	<script src="js/waypoints.min.js"></script>
+	<!-- Portfolio gallery -->
+	<script src="js/imagelightbox.min.js"></script>
+	<!-- Send Email -->
+	<script src="js/sendemail.js"></script>
+</body>
+</html>
